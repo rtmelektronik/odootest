7e0bb3ea6936da9bb5be9777fe42e408bca31eca
rtmelektronik <65658047+rtmelektronik@users.noreply.github.com>
2022-08-15 12:19:06 +0300

    pip test

--- mail_tracking_mailgun/__init__.py
+++ mail_tracking_mailgun/__init__.py
@@ -1,5 +1,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import pip
 
 from . import controllers
 from . import models
 from . import wizards
+
+try:
+    import openupgradelib
+except ImportError:
+    print('\n There was no such module named -openupgradelib- installed')
+    print('xxxxxxxxxxxxxxxx installing openupgradelib xxxxxxxxxxxxxx')
+    pip.main(['install', 'openupgradelib'])
