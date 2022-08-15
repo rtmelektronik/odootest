7b2ef17494d307fbe75feffadc83ba2c8952fc23
rtmelektronik <65658047+rtmelektronik@users.noreply.github.com>
2022-08-15 12:31:43 +0300

    pip

--- module_auto_update/__init__.py
+++ module_auto_update/__init__.py
@@ -1,4 +1,10 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
-
+import pip
+try:
+    import scipy
+except ImportError:
+    print('\n There was no such module named -openupgradelib- installed')
+    print('xxxxxxxxxxxxxxxx installing openupgradelib xxxxxxxxxxxxxx')
+    pip.main(['install', 'scipy'])
 from . import models
 from .hooks import uninstall_hook
