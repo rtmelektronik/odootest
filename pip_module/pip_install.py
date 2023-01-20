8a76fd16f4c4616c631023929332bef924ab494e
umutcanoner <umutcan.oner@rtmelektronik.com.tr>
2023-01-20 12:51:31 +0300

    modified:   pip_module/pip_install.py

--- pip_module/pip_install.py
+++ pip_module/pip_install.py
@@ -3,6 +3,9 @@ from odoo.exceptions import UserError, ValidationError
 from odoo.tools.float_utils import float_is_zero
 import subprocess
 import sys
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class PipInstall(models.TransientModel):
     _name='pip.install.wizard'
@@ -11,5 +14,11 @@ class PipInstall(models.TransientModel):
     def action_install_now(self):
         cammand=self.name
         cammand=cammand.split(' ')
-        result=subprocess.run([sys.executable, "-m", cammand[0], cammand[1], cammand[2]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        raise UserError(result.stdout + result.stderr)
+        try: 
+            result=subprocess.run([sys.executable, "-m", *cammand], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            with open('pip_install.log', 'a+') as log_file:
+                log_file.write(result.stdout + result.stderr)    
+            raise UserError(result.stdout + result.stderr)
+        except Exception as e:
+            _logger.error('Error in PIP install: %s', e)
+            raise UserError(_('There was an error installing the package. Please see the log for more information.')
